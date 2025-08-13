809d0ac14f2d5fb6022e0f967d2e1431dd63f359
SgLy <775150558@qq.com>
2025-08-13 23:20:33 +0800

    feat: directly switch to next ime when have secure permission

--- app/src/main/java/me/sgly/keyboardswitcher/SwitchKeyboardActivity.kt
+++ app/src/main/java/me/sgly/keyboardswitcher/SwitchKeyboardActivity.kt
@@ -1,15 +1,25 @@
 package me.sgly.keyboardswitcher
 
+import android.Manifest
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+
 
 enum class SwitchState {
     Init, SwitchCalled, Switching, SwitchDone
 }
+
+val ids = arrayOf(
+    "com.adamrocker.android.input.simeji/.OpenWnnSimeji",
+    "com.tencent.wetype/.plugin.hld.WxHldService"
+)
 
 class SwitchKeyboardActivity : Activity() {
     private var switchState = SwitchState.Init
@@ -19,13 +29,30 @@ class SwitchKeyboardActivity : Activity() {
 
         Log.d("KeyboardSwitcher", "onCreate")
 
+        val hasPermission = applicationContext.checkSelfPermission(Manifest.permission.WRITE_SECURE_SETTINGS) ==
+                PackageManager.PERMISSION_GRANTED
+
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
             kotlin.run {
                 val im = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                Log.d("KeyboardSwitcher", "call showInputMethodPicker $switchState")
-                im.showInputMethodPicker()
-                switchState = SwitchState.SwitchCalled
+                if (hasPermission) {
+                    val currentId = im.currentInputMethodInfo?.id
+                    val currentIndex = ids.indexOf(currentId)
+                    val nextIndex = (currentIndex + 1) % ids.size
+                    val nextId = ids[nextIndex]
+                    Settings.Secure.putString(
+                        getContentResolver(),
+                        Settings.Secure.DEFAULT_INPUT_METHOD,
+                        nextId
+                    )
+                    Toast.makeText(applicationContext, "Switched to $nextId", Toast.LENGTH_LONG).show()
+                    finish()
+                } else {
+                    Log.d("KeyboardSwitcher", "call showInputMethodPicker $switchState")
+                    im.showInputMethodPicker()
+                    switchState = SwitchState.SwitchCalled
+                }
             }
         }, 500)
     }
